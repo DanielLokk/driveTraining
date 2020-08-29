5dc6ebc4239e02540b9e2ec960562ec27b4543f5
Dani <danielbenet986@outlook.com>
2020-08-29 19:20:26 +0200

    He dintentar treure la X i els kg

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
+import re
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
@@ -8,13 +9,12 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
 client = gspread.authorize(creds)
 
-sheet = client.open("tutorial").sheet1  # Open the spreadhseet
+sheet = client.open("progressio Dani Benet").sheet1  # Open the spreadhseet
 
 data = sheet.get_all_records()  # Get a list of all records
 
-row = sheet.row_values(3)  # Get a specific row
-col = sheet.col_values(3)  # Get a specific column
-cell = sheet.cell(1,2).value  # Get the value of a specific cell
-
-print(row)
-
+day1 = data[1:20:2]
+day2 = data[24:39:2]
+day3 = data[43:58:2]
+day4 = data[63:76:2]
+print(re.findall("X[\d]{1,}kg", str(day2)))
