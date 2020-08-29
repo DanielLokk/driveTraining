ee52a6d0fb3341afcdb28dbe98ddfbaa57c764ff
Dani <danielbenet986@outlook.com>
2020-08-29 18:37:45 +0200

    No sols es conecta sino que funciona

--- train.py
+++ train.py
@@ -1,48 +1,20 @@
-from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
 
-client_id = "729675636971-etnd7b3qip2jv0ksof9eedpfel4dqkg3.apps.googleusercontent.com"
-client_s = "KD6HkoZoyRVF3Dr_szAKGH3e"
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 
-def main():
-     creds = None
-     # The file token.pickle stores the user's access and refresh tokens, and is
-     # created automatically when the authorization flow completes for the first
-     # time.
-     if os.path.exists('token.pickle'):
-          with open('token.pickle', 'rb') as token:
-               creds = pickle.load(token)
-     # If there are no (valid) credentials available, let the user log in.
-     if not creds or not creds.valid:
-          if creds and creds.expired and creds.refresh_token:
-               creds.refresh(Request())
-          else:
-               flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-               creds = flow.run_local_server(port=0)
-          # Save the credentials for the next run
-          with open('token.pickle', 'wb') as token:
-               pickle.dump(creds, token)
+client = gspread.authorize(creds)
 
-     service = build('drive', 'v3', credentials=creds)
+sheet = client.open("tutorial").sheet1  # Open the spreadhseet
 
-     # file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
-     # request = service.files().get_media(fileId=file_id)
-     # fh = io.BytesIO()
-     # downloader = MediaIoBaseDownload(fh, request)
-     # done = False
-     # while done is False:
-     #      status, done = downloader.next_chunk()
-     #      print ("Download %d%%." % int(status.progress() * 100))
-     
+data = sheet.get_all_records()  # Get a list of all records
 
+row = sheet.row_values(3)  # Get a specific row
+col = sheet.col_values(3)  # Get a specific column
+cell = sheet.cell(1,2).value  # Get the value of a specific cell
 
-if __name__ == '__main__':
-    main()
+print(row)
+
